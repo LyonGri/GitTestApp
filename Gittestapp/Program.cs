7836a22a24e78073b773a7e315bdf6c7f37cfad0
Leonid <leogri97@gmail.com>
2020-12-06 19:03:37 +0700

    feature(Program.cs): добавлен новый функционал
     - добавлено отборажение важной информации

--- Gittestapp/Program.cs
+++ Gittestapp/Program.cs
@@ -11,6 +11,7 @@ namespace Gittestapp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("В этой строке содержится новый функционал");
         }
     }
 }
